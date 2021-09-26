339f7eebf218573b795b4a0f8784f5faa1bff648
Matheus Henrique <62672185+matheushrq@users.noreply.github.com>
2021-09-26 20:18:17 -0300

    Add files via upload

---
+++ pratica02.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Matheus Henrique',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
