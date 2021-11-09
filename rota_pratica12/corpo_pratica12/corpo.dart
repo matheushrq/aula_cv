ce410f7fd77296658c4917405b542619ac378c5e
Matheus Henrique <62672185+matheushrq@users.noreply.github.com>
2021-11-08 23:04:05 -0300

    Create corpo.dart

---
+++ rota_pratica12/corpo_pratica12/corpo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Corpo extends StatelessWidget {
+  @override
+  final String texto;
+
+  Corpo(this.texto);
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        '${this.texto}',
+        style: TextStyle(
+            fontSize: 45, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+      padding: EdgeInsets.all(40),
+      margin: EdgeInsets.all(25),
+    );
+  }
+}
