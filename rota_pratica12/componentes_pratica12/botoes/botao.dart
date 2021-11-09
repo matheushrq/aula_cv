80dd927360fcf9f1ef65ada2bc144f97c50a9571
Matheus Henrique <62672185+matheushrq@users.noreply.github.com>
2021-11-08 23:03:28 -0300

    Create botao.dart

---
+++ rota_pratica12/componentes_pratica12/botoes/botao.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Botao extends StatelessWidget {
+  final String proxima;
+  Botao(this.proxima);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          child: Icon(
+            Icons.navigate_next,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              '${this.proxima}',
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class Botoes extends StatelessWidget {
+  final String proxima;
+  Botoes(this.proxima);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ElevatedButton(
+          child: Icon(Icons.navigate_before),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        ElevatedButton(
+          child: Icon(Icons.navigate_next),
+          onPressed: () {
+            Navigator.pushNamed(context, '${this.proxima}');
+          },
+        )
+      ],
+    );
+  }
+}
